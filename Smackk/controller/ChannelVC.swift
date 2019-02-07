45282877df78cc6b9a040eb948385db5736755a5
mohamed <mohamed@faress-mac-3.local>
2019-02-07 09:28:47 +0200

    Smack - SWReveal

--- Smackk/controller/ChannelVC.swift
+++ Smackk/controller/ChannelVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChannelVC.swift
 //  Smackk
 //
 //  Created by mohamed on 2/5/19.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width-60
+    }
+    
+
+    
 
 }
-
---
+++ Smackk/controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Smackk
+//
+//  Created by mohamed on 2/5/19.
+//  Copyright © 2019 mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        //wen we tab the reviel will move left or right
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+
+    }
+    
+
+
+}
